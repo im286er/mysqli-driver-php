8a100cb346a1e394f2c9b248717769143395f7e7
Jian Liu <onanying@126.com>
2017-02-23 12:24:29 +0800

    增加事务，修改bug，重写result

--- Library/MysqliDriver.php
+++ Library/MysqliDriver.php
@@ -4,19 +4,27 @@ namespace Library;
 
 /**
  * mysqli驱动器
- * @author 刘健 <59208859@qq.com>
+ * @author 刘健 <code.liu@qq.com>
  */
 class MysqliDriver
 {
 
+    /* 内部参数 */
     protected $mysqli = '';
+    protected $connectTimeout = 2;
+    protected $transStartStatus = false;
+    protected $transQueryStatus = false;
+
+    /* 用户配置参数 */
     protected $host = 'localhost';
     protected $port = 3306;
     protected $username = 'root';
-    protected $password = '';
-    protected $database = '';
+    protected $password = '123456';
+    protected $database = 'test';
     protected $charset = 'utf8';
     protected $addslashes = true;
+
+    /* 公共参数 */
     public $affectedRows = 0;
 
     public function __construct($config = array())
@@ -43,7 +51,7 @@ class MysqliDriver
             }
         }
         // 连接
-        $this->mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, 2); // 设置超时时间
+        $this->mysqli->options(MYSQLI_OPT_CONNECT_TIMEOUT, $this->connectTimeout); // 设置超时时间
         $connect = @$this->mysqli->real_connect($this->host, $this->username, $this->password, $this->database, $this->port);
         if (!$connect) {
             throw new \Exception(sprintf('Connect Error: [%s] %s', $this->mysqli->connect_errno, $this->mysqli->connect_error));
@@ -64,10 +72,10 @@ class MysqliDriver
         $argv = func_get_args();
         // 取出sql
         $sql = $argv[0];
-        // 处理字符串参数
         unset($argv[0]);
+        // 处理字符串参数        
         foreach ($argv as $key => $value) {
-            if (is_string($value)) {
+            if (!is_numeric($value)) {
                 // 转义
                 if ($this->addslashes) {
                     $argv[$key] = addslashes($argv[$key]);
@@ -89,75 +97,74 @@ class MysqliDriver
         // 返回数据
         if (is_bool($resource)) {
             if (!$resource) {
-                throw new \Exception(sprintf("Error SQL: [%s] %s", $this->mysqli->errno, $this->mysqli->error));
+                // sql执行失败
+                if ($this->transStartStatus) {
+                    $this->transQueryStatus = false;
+                } else {
+                    throw new \Exception(sprintf("Error SQL: [%s] %s", $this->mysqli->errno, $this->mysqli->error));
+                }
             }
             return $resource;
         } else {
-            $this->affected_rows = 0;
             return new MysqliResult($resource);
         }
     }
 
-}
-
-/**
- * mysqli结果集
- * @author 刘健 <59208859@qq.com>
- */
-class MysqliResult
-{
-
-    protected $resource = '';
-    public $numRows = '';
-
-    public function __construct($resource)
+    /**
+     * 自动事务开始
+     */
+    public function transStart()
     {
-        $this->resource = $resource;
-        $this->numRows = $resource->num_rows;
+        $this->transStartStatus = true;
+        $this->transQueryStatus = true;
     }
 
     /**
-     * 返回全部结果集，对象
-     * @return [type] [description]
+     * 自动事务完成 (自动提交或回滚)
      */
-    public function allObject()
+    public function transComplete()
     {
-        $tmp = array();
-        while ($row = $this->rowObject()) {
-            $tmp[] = $row;
+        if ($this->transQueryStatus) {
+            $this->transCommit();
+        } else {
+            $this->transRollback();
         }
-        return $tmp;
+        $this->transStartStatus = false;
+        $this->transQueryStatus = false;
     }
 
     /**
-     * 返回全部结果集，数组
-     * @return [type] [description]
+     * 事务执行状态
      */
-    public function allArray()
+    public function transStatus()
     {
-        $tmp = array();
-        while ($row = $this->rowArray()) {
-            $tmp[] = $row;
-        }
-        return $tmp;
+        return $this->transQueryStatus;
     }
 
     /**
-     * 返回结果集的一行，对象
-     * @return [type] [description]
+     * 事务开始
      */
-    public function rowObject()
+    public function transBegin()
     {
-        return $this->resource->fetch_object();
+        $this->mysqli->autocommit(false); // 关闭自动提交
     }
 
     /**
-     * 返回结果集的一行，数组
-     * @return [type] [description]
+     * 事务提交
      */
-    public function rowArray()
+    public function transCommit()
     {
-        return $this->resource->fetch_assoc();
+        $this->mysqli->commit();
+        $this->mysqli->autocommit(true); // 重新开启自动提交
+    }
+
+    /**
+     * 事务回滚
+     */
+    public function transRollback()
+    {
+        $this->mysqli->rollback();
+        $this->mysqli->autocommit(true); // 重新开启自动提交
     }
 
 }
